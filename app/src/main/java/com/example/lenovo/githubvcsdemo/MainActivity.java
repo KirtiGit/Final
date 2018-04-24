70dcbf51e70a67e29cb22f41fb1ac6c84be2dfab
KirtiGit <kirti.srivastav1993@gmail.com>
2018-04-25 00:23:48 +0530

    Final Testing

---
+++ app/src/main/java/com/example/lenovo/githubvcsdemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.lenovo.githubvcsdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    public void  aVoid(View view)
+    {
+        Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+    }
+}
